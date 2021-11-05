739a9efb4eb47ea952434b4ac80622065a7a6969
uenji <eunji970@naver.com>
2021-11-05 21:30:27 +0900

    fix: Update section page-scroll
    페이지 스크롤 attr 변경
    
    #5

--- resources/js/common.js
+++ resources/js/common.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		event.preventDefault();
 		var $anchor = $(this);
 		$('html, body').stop().animate({
-			scrollTop: ($($anchor.attr('href')).offset().top)
+			scrollTop: ($($anchor.attr('id')).offset().top)
 		}, 1250, 'easeInOutExpo');
 	});
 	
